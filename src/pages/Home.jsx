a7d337bc5b53889b0b595f4bf9b39b6914d7d60c
DEEP PATEL <deep.patel8155@gmail.com>
2025-01-03 21:55:26 +0530

    initial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        home page bhia
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/MyAppointment.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MyAppointment = () => {
+  return (
+    <div>MyAppointment</div>
+  )
+}
+
+export default MyAppointment
+
